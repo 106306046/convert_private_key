af3da4e1ece311d1398bccca496fa3c7c6794d0b
106306046 <timmy199922@gmail.com>
2023-03-07 14:10:18 +0800

    init vue cli

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <PrivateKeyConverter />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import PrivateKeyConverter from './components/PrivateKeyConverter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    PrivateKeyConverter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PrivateKeyConverter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="hello">
+        <h1>Input Your Private Key</h1>
+        <v-text-field label="Password" type="input" hint="Enter your password to access this website"></v-text-field>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'PrivateKeyConverter',
+    props: {
+    }
+}
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
+  
